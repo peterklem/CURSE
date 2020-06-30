cb40268ec22ff4501b64cdd093179ee46c8814f1
peterklem <klembczykp@wit.edu>
2020-06-30 15:01:15 -0400

    Started code and testing on Individual.login() method

--- Individual.py
+++ Individual.py
@@ -1,29 +1,99 @@
+import sqlite3
+
 class Individual:
     '''Holds first name, last name, and ID'''
 
     # Variables
-    __first_name = ""
-    __last_name = ""
-    __id = ""
+    _first_name = ""
+    _last_name = ""
+    _id = ""
 
     #   Methods
     def __init__(self):  # No arguments passed
-        print("A new individual has been created")  # Print confirmation
+        '''Initializes an individual'''
 
-    def set_first_name(self, first_name):
+
+
+    def set_first_name(self):
         '''Sets first name of individual'''
-        self.__first_name = first_name
+        self._first_name = input("Enter first name: ")
+        print("First name set as " + self._first_name)
 
-    def set_last_name(self, last_name):
+
+    def set_last_name(self):
         '''Sets last name of individual'''
-        self.__last_name = last_name
+        self._last_name = input("Enter last name: ")
+        print("Last name set as " + self._last_name)
 
-    def set_id(self, id):
+
+    def set_id(self):
         '''Sets ID of individual'''
-        self.__id = id
+        id_flag = False     # True if the input ID is not a duplicate
+
+        while id_flag == False:     # Loop until a valid ID is input
+
+            input_id = input("Enter individual ID: ")
+            
+            # Check for matching ID, must be unique
+
+            db = sqlite3.connect('database.db')
+            cursor = db.cursor()
+            cmd = 'SELECT ID FROM STUDENT WHERE ID = ' + input_id
+            cursor.execute(cmd)
+            matching_id = cursor.fetchall()
+
+            if len(matching_id) == 0:   # Check ID flag
+                self._id = input_id
+                print("ID set.")  # Print confirmation
+                id_flag = True
+
+            else:
+                print("That ID is already taken. Please choose another")
+                id_flag = False
+
+
+        db.close()
+        self._id = id
 
     def print_name_id(self):
         '''Prints all three variables to the output'''
-        print('First Name: ' + self.__first_name)
-        print("Last Name: " + self.__last_name)
-        print("ID: " + self.__id)
+        print('First Name: ' + self._first_name)
+        print("Last Name: " + self._last_name)
+        print("ID: " + self._id)
+
+
+    def login(self):
+        class_type = "" # holds user type
+
+        input_email = input("Enter user email (without '@wit.edu'): ")
+        input_id = input("Enter user ID: ")
+        
+        print(type(self))
+
+        if isinstance(class_type, Student.Student):
+            class_type = 'STUDENT'
+
+        elif type(self) == "<class 'Instructor.Instructor'>":
+            class_type = 'INSTRUCTOR'
+
+        elif type(self) == "<class 'Admin.Admin'>":
+            class_type = 'ADMIN'
+
+        else:
+            print('You are not registered as a student, instructor, or admin. Please contact CURSE support for assistance.')
+            return False
+
+        # Query database
+        db = sqlite3.connect('database.db')
+        cursor = db.cursor()
+
+        cmd = "SELECT EMAIL, ID FROM " + class_type + " WHERE EMAIL='" + input_email + "';"
+        cursor.execute(cmd)
+        data = cursor.fetchall()
+
+        print(data)
+        db.close()
+
+
+    def logout(self):
+        pass
--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import Individual
+import Student
+import Instructor
+import Admin
+
 if __name__ == '__main__':
+
+    #individual1 = Individual.Individual()
+
+    #indiv = Individual.Individual()
+    #indiv.login()
+
+
+    stu = Student.Student()
+    stu.login()
     
