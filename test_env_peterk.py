71fd250a6ee4d68b54f9a5e318ea1a243402b33a
peterklem <klembczykp@wit.edu>
2020-07-05 15:17:50 -0400

    Added test environment

---
+++ test_env_peterk.py
@@ -0,0 +1,10 @@
+from Individual import *
+from Student import *
+from Instructor import *
+from Admin import *
+import sqlite3
+
+if __name__ == '__main__':
+
+    stud = Student()
+    stud.course_search_id('ELEC-3725-02')
