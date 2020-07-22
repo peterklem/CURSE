6855190bfaa96e8e1f78140b92eca22bb31cba6b
peterklem <klembczykp@wit.edu>
2020-07-22 19:57:48 -0400

    added print schedule and check conflicts fn's

--- Student.py
+++ Student.py
@@ -4,15 +4,9 @@ from Individual import *
 class Student(Individual):
     '''Contains functions that pertain to students'''
 
-    def __init__(self):
-        self._schedule = []  # Student schedule
-        print('A new student has been created')
-
-    def search_course(self, course_name):
-        '''Searches courses by name'''
-        results = []
-        # Search SQL database for matches, add to results
-        return results
+    def __init__(self, id):
+        self.__id = id # Student ID
+        #print('A new student has been created')
 
     def add_course(self, course_id):
         '''Adds this student to a course specified by course ID'''
@@ -80,7 +74,72 @@ class Student(Individual):
         userRemoveFinal = ('UPDATE COURSE SET STUDENT_LIST = \'' + newString + '\' WHERE CRN = ' + courseRemove)
         cursor.execute(userRemoveFinal)
 
+    def check_conflicts(self):
+        '''Checks the schedule for conflicts'''
+        # Get courses that student is taking 
+        db = sqlite3.connect('assignment2.db')
+        cursor = db.cursor()
+        command = "SELECT COURSES FROM STUDENT WHERE ID = " + str(self.__id) + ";"
+        cursor.execute(command)
+
+        all_crn = cursor.fetchall()
+        crn_split = all_crn[0][0].split() # Split with spaces
+        
+        reg_courses = []
+        for crn in crn_split: # Loop through courses
+            command = "SELECT TITLE, START_TIME, END_TIME, DAYS_OF_WEEK FROM COURSE WHERE CRN = " + crn + ";"
+            cursor.execute(command)
+            course_data = cursor.fetchall()
+            course_dict = {}
+            course_dict['title'] = course_data[0][0]
+            course_dict['start_time'] = course_data[0][1]
+            course_dict['end_time'] = course_data[0][2]
+            course_dict['days_of_week'] = course_data[0][3].split()
+            reg_courses.append(course_dict) # Add to registered courses list
+        
+        for i in range(len(reg_courses)):
+            for j in range(i + 1, len(reg_courses)):
+                # Check day of week
+                for day in reg_courses[i]['days_of_week']:
+                    if day in reg_courses[j]['days_of_week']:
+                        # Same day, Check start times and end times
+                        if reg_courses[i]['start_time'] >= reg_courses[j]['start_time'] and reg_courses[i]['start_time'] <= reg_courses[j]['end_time']:
+                            print('Conflict between ' + reg_courses[i]['title'] + ' and ' + reg_courses[j]['title'] + '. Day: ' + day)
+                        elif reg_courses[j]['start_time'] >= reg_courses[i]['start_time'] and reg_courses[j]['start_time'] <= reg_courses[i]['end_time']:
+                            print('Conflict between ' + reg_courses[i]['title'] + ' and ' + reg_courses[j]['title'] + '. Day: ' + day)
+            
+        db.close()
+
+
     def print_schedule(self):
-        '''Prints out student's schedule'''
-        for i in self._schedule:
-            print(i)
+        '''Prints student schedule'''
+
+        print('SCHEDULE FOR STUDENT ' + str(self.__id) + ': \n\n')
+        db = sqlite3.connect('assignment2.db')
+        cursor = db.cursor()
+        command = "SELECT COURSES FROM STUDENT WHERE ID = " + str(self.__id) + ";"
+        cursor.execute(command)
+
+        student_course_ids = cursor.fetchall()
+        course_id_list = student_course_ids[0][0].split()
+
+        for crn in course_id_list:
+            command = "SELECT * FROM COURSE WHERE CRN = " + crn + ";"
+            cursor.execute(command)
+            course_data = cursor.fetchall()
+            print('Title: ' + course_data[0][0])
+            print('CRN: ' + str(course_data[0][1]))
+            print('Department: ' + course_data[0][2])
+            print('Instructor: ' + course_data[0][3])
+            print('Start Time: ' + str(course_data[0][4]))
+            print('End Time: ' + str(course_data[0][5]))
+            print('Days: ' + course_data[0][6])
+            print('Semester: ' + course_data[0][7])
+            print('Credits: ' + str(course_data[0][8]) + '\n\n')
+
+        db.close()
+            
+            
+
+
+
